7e29ad900146d337761eb3f6695a13b80619cf85
miquelmiro3 <miquel.miro3@gmail.com>
2022-07-16 10:18:43 +0200

    GameManager skeleton

--- Assets/Scripts/DiceTosser.cs
+++ Assets/Scripts/DiceTosser.cs
@@ -62,29 +62,34 @@ public class DiceTosser : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            for (int i = 0; i < (int)DICES.COUNT; ++i)
-            {
-                int difference = Dices[i].Count - DiceAmounts[i];
-                if (difference > 0)
-                {
-                    for (int j = 0; j < difference; ++j)
-                    {
-                        Destroy(Dices[i][Dices[i].Count - 1]);
-                        Dices[i].RemoveAt(Dices[i].Count - 1);
-                    }
-                }
-                else if (difference < 0)
-                {
-                    difference *= -1;
-                    for (int j = 0; j < difference; ++j)
-                    {
-                        Dices[i].Add(Instantiate(DicePrefabs[i]).GetComponent<Dice>());
-                    }
-                }
+            TossDices(DiceAmounts);
+        }
+    }
 
-                for (int j = 0; j < Dices[i].Count; ++j)
-                    Dices[i][j].TossDice();
+    public void TossDices(int[] diceAmounts)
+    {
+        for (int i = 0; i < (int)DICES.COUNT; ++i)
+        {
+            int difference = Dices[i].Count - diceAmounts[i];
+            if (difference > 0)
+            {
+                for (int j = 0; j < difference; ++j)
+                {
+                    Destroy(Dices[i][Dices[i].Count - 1]);
+                    Dices[i].RemoveAt(Dices[i].Count - 1);
+                }
             }
+            else if (difference < 0)
+            {
+                difference *= -1;
+                for (int j = 0; j < difference; ++j)
+                {
+                    Dices[i].Add(Instantiate(DicePrefabs[i]).GetComponent<Dice>());
+                }
+            }
+
+            for (int j = 0; j < Dices[i].Count; ++j)
+                Dices[i][j].TossDice();
         }
     }
 
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GAME_STATES
+{
+    ROLL_STAGE = 0,
+    PLACEMENT = 1,
+    COMBAT = 2,
+    DAMAGE_PHASE = 3,
+    COUNT = 4
+}
+
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static GameManager Instance;
+
+    public DiceTosser DiceTosser;
+
+    public int[] DiceAmounts;
+
+    GAME_STATES GameState;
+
+    void Awake()
+    {
+        if (Instance != null)
+            Destroy(this);
+        else
+            Instance = null;
+    }
+
     void Start()
+    {
+        GameState = GAME_STATES.ROLL_STAGE;
+        ChangeState();
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void EnterState()
     {
-        
+        switch (GameState)
+        {
+            case GAME_STATES.ROLL_STAGE:
+                DiceTosser.TossDices(DiceAmounts);
+                break;
+            case GAME_STATES.PLACEMENT:
+                break;
+            case GAME_STATES.COMBAT:
+                break;
+            case GAME_STATES.DAMAGE_PHASE:
+                break;
+        }
+    }
+
+    void ChangeState()
+    {
+        GameState = (GAME_STATES)(((int)GameState + 1) % (int)GAME_STATES.COUNT);
+        EnterState();
     }
 }
